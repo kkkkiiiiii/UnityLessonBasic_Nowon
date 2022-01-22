41cc853894d336ee531c634a64713fce679e1556
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2022-01-22 18:49:09 +0900

    1241
    erer

---
+++ UnityLesson_CSharp_/UnityLesson_CSharp_If/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UnityLesson_CSharp_If
+{
+    internal class Program
+    {
+        static bool condition1=false;     
+        static bool condition2=true;
+        static bool condition3=false;
+        static void Main(string[] args)
+        {
+            
+
+            if (condition1)
+            {
+                Console.WriteLine("condition1 true!");
+            }
+            else if(condition2)
+            {
+                Console.WriteLine("condition1 false, condition2 true");
+            }
+            else if (condition3)
+            {
+                Console.WriteLine("Hello World!3");
+            }
+            else
+            {
+
+            }
+        }
+        
+    }
+}
---
+++ UnityLesson_CSharp_/UnityLesson_CSharp_ProgramClass_Dotnet5/Program.cs
@@ -0,0 +1,30 @@
+﻿//파란색 글자 : 키워드, 예약어
+
+//using
+//1. 미리 짜여진 코드 덩어리
+//2. 우선순의 차순위인 함수를 일반적으로 호출할 때
+using System;
+
+// namespace 키워드
+// 클래스간의 이름 충돌을 방지하면서 코드를 한 공간으로 묶는 기능.
+namespace UnityLesson_CSharp_ProgramClass_Dotnet5 /*프로젝트이름*/
+{
+    //Main 함수가 Program 클래스 안에 들어있는 이유
+    //객체지향프로그래밍 컨셉을 지키기 위함
+    internal class Program
+    {
+        //Main함수 
+        //.exe (실행 파일) 을 실행하면 제일 처음 호출되는 함수
+
+        //static(정적) <-> dynamic(동적)
+        //소프트웨어에서 메모리에 할당이 가능한 지 여부로 구분
+        //static정의하면 유일무이한 개념이된다.
+
+        //void : '아무것도 없음' 을 뜻한다.
+        //함수에서는 반환형이 없다.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
